5ec9a0c3467acffe58b09575c8a3b20af4901878
keshavkabrakk2@gmail.com <keshavkabrakk2@gmail.com>
2020-03-30 17:33:44 +0530

    intresting number

--- intresting.cpp
+++ intresting.cpp
@@ -1,11 +1,9 @@
 #include<iostream>
-#include<stack>
 using namespace std;
-stack<int> s1;
-stack<int> s2;
+
 int main()
 {
-    int first_number, second_number,pushed, digits =0, odd_digits=0 , even_digits =0,original_number, count =0, value;
+    int first_number, second_number, odd_digits=0 , even_digits =0, count =0, value;
     cout<<"enter the range\n ";
      cin>>first_number;
      cin>>second_number;
@@ -15,14 +13,6 @@ int main()
             digits =0;
             even_digits = 0;
             odd_digits =0;
-            while(number>0)
-            {
-                //count num of digits in number
-                number=number/10;
-                digits++;
-                
-            }
-            number = i;
             while(number>0)
             {
                 value = number%10;
@@ -53,47 +43,3 @@ int main()
 
 
 
-
-
-/*
-i = original_number;
-        cout<<"value "<<i<<"\n";
-        if(digits % 2 == 0)
-        {
-            while(i>0)
-            {
-            even_digits++;
-            i=i/10;
-
-            if(i<0) break;
-            odd_digits++;
-            i= i/10;
-            }
-            
-
-        }
-        else
-        {
-            while(i>0)
-            {
-            i= i/10;
-            odd_digits++;
-            // thn even 
-            if(i<0) break;
-            even_digits++;
-            i=i/10;
-            }
-
-        }
-        cout<<"comming here";
-
-        if(even_digits %2 != 0 && odd_digits %2 == 0)
-        {
-            cout<<original_number;
-            count ++;
-        }
-
-    }
-    cout<<"total numbers of number are" <<count<<"\n";
-    return 0;
-    */
